143c29645df39a06e534f65b9909cf5fdf11e6cc
paticow <patimatanazareth@gmail.com>
2025-06-01 00:27:46 -0400

    views updated

--- views/login.php
+++ views/login.php
@@ -7,13 +7,12 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Zain:ital,wght@0,200;0,300;0,400;0,700;0,800;0,900;1,300;1,400&display=swap" rel="stylesheet">
-<link rel="website icon" type="png" href="../images/login-lock.png">
+<link rel="website icon" type="png" href="../images/login-lock-w.png">
 <link rel="stylesheet" href="../styles/login.css" />
 
 </head>
 <body>
   <div class="outer-container">
-    <!-- Panel izquierdo: Login -->
     <div class="left-pane">
       <form action="../control/control_login.php" method="post">
         <h2>Inicio de sesión</h2>
@@ -21,13 +20,11 @@
         <input type="text" name="usuario" id="usuario" pattern="[a-zA-Z0-9]{4,10}" required>
         <label for="clave">Contraseña:</label>
         <input type="password" name="clave" id="clave" pattern="[a-zA-Z0-9@#$%&-_+*/]{4,20}" required>
-        <button type="submit">Login</button>
+        <button type="submit">Iniciar</button>
       </form>
       <h4>¿No tienes cuenta?</h4><a class="create-user" href="signup.php">Haz click aquí para crearla</a>
     </div>
-    <!-- Panel derecho: Imagen decorativa -->
     <div class="right-pane">
-
     </div>
   </div>
 </body>
--- views/signup.php
+++ views/signup.php
@@ -7,13 +7,12 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Zain:ital,wght@0,200;0,300;0,400;0,700;0,800;0,900;1,300;1,400&display=swap" rel="stylesheet">
-<link rel="website icon" type="png" href="../images/user-pen.png">
+<link rel="website icon" type="png" href="../images/user-pen-w.png">
 <link rel="stylesheet" href="../styles/signup.css" />
 </head>
 
 <body>
   <div class="outer-container">
-    <!-- Panel izquierdo: Login -->
     <div class="left-pane">
       <form action="../controllers/signup.php" method="post">
         <h2>Crear cuenta</h2>
@@ -21,11 +20,10 @@
         <input type="text" name="alias" id="alias" pattern="[a-zA-Z0-9]{4,10}" required>
         <label for="clave">Contraseña:</label>
         <input type="password" name="clave" id="clave" pattern="[a-zA-Z0-9@#$%&-_+*/]{4,20}" required>
-        <button type="submit">Login</button>
+        <button type="submit">Crear</button>
       </form>
       <h4>¿Ya tienes cuenta?</h4><a class="create-user" href="login.php">Haz click aquí para ingresar</a>
     </div>
-    <!-- Panel derecho: Imagen decorativa -->
     <div class="right-pane">
 
     </div>
