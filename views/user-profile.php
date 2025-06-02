4bdfbf08a7f9a659b6c60564491c3dca8fe2de7a
paticow <patimatanazareth@gmail.com>
2025-06-01 23:39:49 -0400

    Add user page and its CSS

--- views/user-profile.php
+++ views/user-profile.php
@@ -4,69 +4,89 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Zain:ital,wght@0,200;0,300;0,400;0,700;0,800;0,900;1,300;1,400&display=swap" rel="stylesheet">
+    <link rel="website icon" type="png" href="../images/user.png">
     <link rel="stylesheet" href="../styles/user-profile.css">
 </head>
 <body>
     <div id="container">
-        <div id="lside-bar">
-            <div class="lcontent" id="ltitle">Contenido</div>
-            <div class="lcontent">Nueva idea</div>
-            <div class="lcontent">Foro</div>
-            <div class="lcontent">Guardados</div>
-            <div class="lcontent">Likes</div>
-        </div>
+<div id="lside-bar">
+    <div id="ltitle">
+        <span class="icon-title"></span> Contenido
+    </div>
+    <a class="lcontent idea">
+        <span class="icon"></span> Nueva idea
+    </a>
+    <a class="lcontent forum">
+        <span class="icon"></span> Foro
+    </a>
+    <a class="lcontent saved">
+        <span class="icon"></span> Guardados
+    </a>
+    <a class="lcontent likes">
+        <span class="icon"></span> Likes
+    </a>
+</div>
         <div id="center">
             <div id="info-container">
                 <div id="up-title">Datos de usuario</div>
                 <div id="data-container">
-        <div id="username-info">
-            <p>Nombre de usuario: <span id="username">holas</span></p>
-            <button onclick="mostrarInput('new-user')">Modificar nombre</button> 
-            <div id="new-user" style="display: none;">
-                <form action="procesar.php" method="POST">
-                    <input type="text" name="nuevo_nombre" placeholder="Nuevo nombre">
-                    <button type="submit">Guardar</button>
-                    <button type="button" onclick="cerrarFormulario('new-user')">Cerrar</button>
-                </form>
-            </div>
-        </div>
+                    <div id="username-info">
+                        <p>Nombre de usuario: <span id="username">holas</span></p>
+                        <button class="bttn-modificar" onclick="toggleInput('new-user')">Modificar</button> 
+                        <div id="new-user">
+                            <form action="procesar.php" method="POST">
+                                <input type="text" name="nuevo_nombre" placeholder="Nuevo usuario">
+                                <button type="submit" class="save-bttn">Guardar</button>
+                                <button type="button" onclick="toggleInput('new-user')" class="exit-bttn">Cerrar</button>
+                            </form>
+                        </div>
+                    </div>
 
-        <div id="password-info">
-            <p>Contraseña: <span id="password">********</span></p>
-            <button onclick="mostrarInput('new-password')">Modificar contraseña</button> 
-            <div id="new-password" style="display: none;">
-                <form action="procesar.php" method="POST">
-                    <input type="password" name="nuevo_password" placeholder="Nueva contraseña" required>
-                    <button type="submit">Guardar</button>
-                    <button type="button" onclick="cerrarFormulario('new-password')">Cerrar</button>
-                </form>
-            </div>
-        </div>
-        
-                <div id="elim-user">
-        <form id="deleteForm" action="eliminar.php" method="POST">
-            <button type="button" onclick="confirmarEliminacion()">Borrar cuenta</button>
-        </form>
-    </div>
+                    <div id="password-info">
+                        <p>Contraseña: <span id="password">********</span></p>
+                        <button class="bttn-modificar" onclick="toggleInput('new-password')">Modificar</button> 
+                        <div id="new-password">
+                            <form action="procesar.php" method="POST">
+                                <input type="password" name="nuevo_password" placeholder="Nueva contraseña" required>
+                                <button type="submit" class="save-bttn">Guardar</button>
+                                <button type="button" onclick="toggleInput('new-password')" class="exit-bttn">Cerrar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
 
-    <script>
-        function confirmarEliminacion() {
-            if (confirm("¿Estás seguro de que deseas eliminar tu cuenta? Esta acción es irreversible.")) {
-                document.getElementById("deleteForm").submit();
-            }
-        }
-    </script>
-    </div>
+                <div id="down-bttns">
+                    <form id="deleteForm" action="eliminar.php" method="POST">
+                        <button class="low-bttn" type="button" onclick="confirmarEliminacion()">Borrar cuenta</button>
+                    </form>
+                    <button class="low-bttn exit-bttn"  type="button">Salir</button>
+                </div>
 
-    <script>
-        function mostrarInput(id) {
-            document.getElementById(id).style.display = "block";
-        }
+                <script>
+                    function confirmarEliminacion() {
+                        if (confirm("¿Estás seguro de que deseas eliminar tu cuenta? Esta acción es irreversible.")) {
+                            document.getElementById("deleteForm").submit();
+                        }
+                    }
 
-        function cerrarFormulario(id) {
-            document.getElementById(id).style.display = "none";
-        }
-    </script>
+                    function toggleInput(id) {
+                        const element = document.getElementById(id);
+                        if (!element.classList.contains("show")) {
+                            element.style.display = "block";
+                            setTimeout(() => {
+                                element.classList.add("show");
+                            }, 10);
+                        } else {
+                            element.classList.remove("show");
+                            setTimeout(() => {
+                                element.style.display = "none";
+                            }, 500);
+                        }
+                    }
+                </script>
             </div>
         </div>
     </div>
